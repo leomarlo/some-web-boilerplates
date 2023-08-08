4d88499ad77f3fb5ddbbbb67fb97c3b132025b7f
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-08-09 00:59:31 +0300

    working boilerplate react app

--- webpack.common.js
+++ webpack.common.js
@@ -6,10 +6,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      }
+      }      
     ]
   },
   resolve: {
