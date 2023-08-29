4f43e027e241087d73c6824251ae3b1d19614672
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-08-29 10:42:02 +0300

    added two environment variables to the webpack, so that they can be recognized

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -30,6 +31,10 @@ module.exports = {
     }),
     new Dotenv({
       path: './frontend.env'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_DEVELOPMENT_MODE': JSON.stringify(process.env.REACT_APP_DEVELOPMENT_MODE),
+      'process.env.REACT_APP_DOCKERIZED': JSON.stringify(process.env.REACT_APP_DOCKERIZED),
     })
   ]
     
